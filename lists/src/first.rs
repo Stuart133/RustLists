73bd8442d745ddd33025e11574b0d6ddb7fe6b3b
Stuart Reid <stuart1334@gmail.com>
2022-04-12 17:01:51 +0100

    Add some list methods

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,9 +1,38 @@
-struct Node {
-  elem: i32,
-  next: List,
+use std::mem;
+
+pub struct List {
+    head: Link,
 }
 
-pub enum List {
-  Empty,
-  More(Box<Node>)
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.head {
+            Link::Empty => {}
+            Link::More(node) => {}
+        };
+        unimplemented!();
+    }
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
 }

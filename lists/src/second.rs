d4d8c451533dea60cad30cdafb430104a3a743f2
Stuart Reid <stuart1334@gmail.com>
2022-04-16 18:04:52 +0100

    Add into iter method

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -2,6 +2,8 @@ pub struct List<T> {
     head: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
@@ -12,9 +14,20 @@ impl<T> Drop for List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
+  }
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+      IntoIter(self)
     }
 
     pub fn push(&mut self, elem: T) {
@@ -91,5 +104,25 @@ mod test {
 
       assert_eq!(list.peek(), Some(&3));
       assert_eq!(list.peek_mut(), Some(&mut 3));
+      list.peek_mut().map(|value| {
+        *value = 42;
+      });
+
+      assert_eq!(list.peek(), Some(&42));
+      assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+      let mut list = List::new();
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      let mut iter = list.into_iter();
+      assert_eq!(iter.next(), Some(3));
+      assert_eq!(iter.next(), Some(2));
+      assert_eq!(iter.next(), Some(1));
+      assert_eq!(iter.next(), None);
     }
 }

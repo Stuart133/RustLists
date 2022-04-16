91100f938ddfa9071c653299aca8fc37ee772707
Stuart Reid <stuart1334@gmail.com>
2022-04-16 17:58:15 +0100

    Add peek method

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -32,6 +32,18 @@ impl<T> List<T> {
           node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+      self.head.as_ref().map(|node| {
+        &node.elem
+      })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+      self.head.as_mut().map(|node| {
+          &mut node.elem
+      })
+  } 
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -66,5 +78,18 @@ mod test {
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+      let mut list = List::new();
+      assert_eq!(list.peek(), None);
+      assert_eq!(list.peek_mut(), None);
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      assert_eq!(list.peek(), Some(&3));
+      assert_eq!(list.peek_mut(), Some(&mut 3));
     }
 }

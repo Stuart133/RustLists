f6cef0cb9ffb41edac760919b6ddf23f8135c298
Stuart Reid <stuart1334@gmail.com>
2022-04-20 17:33:24 +0100

    Use raw pointers everywhere

--- lists/src/fifth.rs
+++ lists/src/fifth.rs
@@ -8,43 +8,53 @@ pub struct List<T> {
 impl<T> List<T> {
     pub fn new() -> List<T> {
         List {
-            head: None,
+            head: ptr::null_mut(),
             tail: ptr::null_mut(),
         }
     }
 
     pub fn push(&mut self, elem: T) {
-        let mut new_tail = Box::new(Node {
-            elem: elem,
-            next: None,
-        });
+        unsafe {
+            let new_tail = Box::into_raw(Box::new(Node {
+                elem: elem,
+                next: ptr::null_mut(),
+            }));
 
-        let raw_tail: *mut _ = &mut *new_tail;
+            if !self.tail.is_null() {
+                (*self.tail).next = new_tail;
+            } else {
+                self.head = new_tail;
+            }
 
-        if !self.tail.is_null() {
-            unsafe { (*self.tail).next = Some(new_tail) };
-        } else {
-            self.head = Some(new_tail);
+            self.tail = new_tail
         }
-
-        self.tail = raw_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-      self.head.take().map(|head| {
-        let head = *head;
-        self.head = head.next;
+        unsafe {
+            if self.head.is_null() {
+                None
+            } else {
+                let head = Box::from_raw(self.head);
+                self.head = head.next;
 
-        if self.head.is_none() {
-          self.tail = ptr::null_mut();
+                if self.head.is_null() {
+                    self.tail = ptr::null_mut();
+                }
+
+                Some(head.elem)
+            }
         }
-
-        head.elem
-      })
     }
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop() {}
+    }
+}
+
+type Link<T> = *mut Node<T>;
 
 struct Node<T> {
     elem: T,

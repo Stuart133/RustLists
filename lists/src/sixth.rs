397c1f41e9dc456e70af5e7da084c3d2a9c0e31a
Stuart Reid <stuart1334@gmail.com>
2022-04-21 10:51:25 +0100

    Start the final list

---
+++ lists/src/sixth.rs
@@ -0,0 +1,15 @@
+use std::usize;
+
+pub struct LinkedList<T> {
+  front: Link<T>,
+  back: Link<T>,
+  len: usize
+}
+
+type Link<T> = *mut Node<T>;
+
+struct Node<T> {
+  front: Link<T>,
+  back: Link<T>,
+  elem: T,
+}
